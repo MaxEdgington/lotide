b682487e9794b7fe617cebf0f47409426f8e6117
MaxEdgington <edgington.m.w@gmail.com>
2023-07-26 15:28:44 -0400

    finished implement letterPositions f0r lotide project

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+
+const assertArraysEqual = (a,b) => {
+  if (a.toString() === b.toString()) {
+    console.log(`✅✅✅Assertion Passed: [${a}] === [${b}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: [${a}] !== [${b}]`);
+        
+  }
+};
+  
+const letterPositions = function(str) {
+  const strLowercase = str.toLowerCase();
+  
+  const result = {};
+  
+  /*
+  
+    for (let letter of strLowercase) {
+      if (result[letter] === undefined) {
+        result[letter] = 1;
+      } else {
+        result[letter]++;
+      }
+    }
+  
+   */
+    
+  for (let i = 0; i < strLowercase.length; i++) {
+    if (strLowercase[i] !== " ") {
+      console.log(i);
+      if (result [strLowercase[i]] === undefined) {
+          
+        result[strLowercase[i]] = [i];
+      } else {
+        result[strLowercase[i]].push(i);
+      }
+    }
+  }
+  return result;
+};
+  
+  
+  
+console.log(letterPositions("check me out!"));
+
+assertArraysEqual(letterPositions("hello").e, [1]);
