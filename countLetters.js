3d22a1b87ed88e466e92d007a3584fcd9647cc7d
MaxEdgington <edgington.m.w@gmail.com>
2023-07-26 20:56:53 -0400

    first solve for eqObjects.js

--- countLetters.js
+++ countLetters.js
@@ -10,17 +10,36 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(str) {
   const strLowercase = str.toLowerCase();
-
+  
   const result = {};
-
-  for (let letter of strLowercase) {
-    if (result[letter] === undefined) {
-      result[letter] = 1;
-    } else {
-      result[letter]++;
+  
+  /*
+  
+    for (let letter of strLowercase) {
+      if (result[letter] === undefined) {
+        result[letter] = 1;
+      } else {
+        result[letter]++;
+      }
+    }
+  
+   */
+    
+  for (let i = 0; i < strLowercase.length; i++) {
+    if (strLowercase[i] !== " ") {
+      console.log(i);
+      if (result [strLowercase[i]] === undefined) {
+          
+        result[strLowercase[i]] = [i];
+      } else {
+        result[strLowercase[i]].push(i);
+      }
     }
   }
   return result;
 };
-
+  
+  
+  
 console.log(countLetters("check me out!"));
+assertArraysEqual(letterPositions("hello").e, [1]);
---
+++ eqObjects.js
@@ -0,0 +1,56 @@
+const eqArrays = (a, b) => {
+  return a.toString() === b.toString();
+};
+  
+  
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+  
+};
+  
+  
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+    
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  
+  for (let key of Object.keys(object1)) {
+    if (!object2.hasOwnProperty(key)) {
+      return false;
+    }
+    if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+      return false;
+    }
+    if (Array.isArray(object1[key]) && !eqArrays(object1[key], object2[key])) {
+      return false;
+    }
+  }
+  return true;
+};
+  
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+  
+assertEqual(eqObjects(shirtObject, anotherShirtObject),true); // => true
+  
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+  
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
+  
+assertEqual(1, 1);
+  
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject),true); // => true
+  
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+  
+assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
