8cbe38ba8f55f1335199d59add84db2a49898350
MaxEdgington <edgington.m.w@gmail.com>
2023-07-31 14:08:46 -0400

    adding any lotide changes

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   if (arr.length > 0) {
@@ -16,5 +8,5 @@ const head = function(arr) {
   }
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
